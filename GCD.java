c3fe370cb089bd11e8c99c71a6cf70563ff29d4f
kojimamomoka <14ec048@ms.dendai.ac.jp>
2015-11-12 12:05:26 +0900

    Create GCD.java

---
+++ GCD.java
@@ -0,0 +1,25 @@
+package algo;
+
+import java.util.Scanner;
+public class GCD {
+  static int gcd(int x, int y) {
+    if (y == 0)
+      return x;
+    else
+      return gcd(y, x % y);
+  }
+
+  public static void main(String[] args) {
+    Scanner stdIn = new Scanner(System.in);
+
+    System.out.println("二つの整数の最大公約数を求めます．");
+
+    System.out.print("整数を入力せよ：");
+    int x = stdIn.nextInt();
+    System.out.print("整数を入力せよ：");
+    int y = stdIn.nextInt();
+
+    System.out.println("最大公約数は" + gcd(x, y) + "です．");
+  }
+}
+

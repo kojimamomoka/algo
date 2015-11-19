ec1a79c2cc50e170a3fc0b53ceb7b97b462c4109
kojimamomoka <14ec048@ms.dendai.ac.jp>
2015-11-20 00:31:35 +0900

    Update QueueSample.java

--- QueueSample.java
+++ QueueSample.java
@@ -1,46 +1,54 @@
 package algo;
+
 import java.util.Scanner;
 public class QueueSample {
-public static void main(String[] args) {
-Scanner stdIn = new Scanner(System.in);
-Queue s = new Queue(100);
-while (true) {
-System.out.println("データ数：" + s.size() + " / "
-+ s.capacity());
-System.out.print("(1)エンキュー (2)デキュー (3)ピーク (4)ダンプ (0)終了：");
-int menu = stdIn.nextInt();
-if (menu == 0) break;
-int x;
-switch (menu) {
-case 1: // エンキュー
-System.out.print("データ：");
-x = stdIn.nextInt();
-try {
-s.enque(x);
-} catch (Queue.OverflowIntQueueException e) {
-System.out.println("キューが満杯です。");
+  public static void main(String[] args) {
+    Scanner stdIn = new Scanner(System.in);
+    Queue s = new Queue(100);
+
+    while (true) {
+      System.out.println("データ数：" + s.size() + " / "+ s.capacity());
+      System.out.print("(1)エンキュー (2)デキュー (3)ピーク (4)ダンプ (0)終了：");
+
+      int menu = stdIn.nextInt();
+      if (menu == 0) break;
+
+      int x;
+      switch (menu) {
+       case 1: // エンキュー
+        System.out.print("データ：");
+        x = stdIn.nextInt();
+        try {
+          s.enque(x);
+        } catch (Queue.OverflowIntQueueException e) {
+          System.out.println("キューが満杯です。");
+        }
+        break;
+
+       case 2: // デキュー
+        try {
+          x = s.deque();
+          System.out.println("デキューしたデータは" + x + "です。");
+        } catch (Queue.EmptyIntQueueException e) {
+          System.out.println("キューが空です。");
+        }
+        break;
+
+       case 3: // ピーク
+        try {
+          x = s.peek();
+          System.out.println("ピークしたデータは" + x + "です。");
+        } catch (Queue.EmptyIntQueueException e) {
+          System.out.println("キューが空です。");
+        }
+        break;
+
+       case 4: // ダンプ
+        s.dump();
+        break;
+      }
+    }
+  }
 }
-break;
-case 2: // デキュー
-try {
-x = s.deque();
-System.out.println("デキューしたデータは" + x + "です。");
-} catch (Queue.EmptyIntQueueException e) {
-System.out.println("キューが空です。");
-}
-break;
-case 3: // ピーク
-try {
-x = s.peek();
-System.out.println("ピークしたデータは" + x + "です。");
-} catch (Queue.EmptyIntQueueException e) {
-System.out.println("キューが空です。");
-}
-break;
-case 4: // ダンプ
-s.dump();
-break;
-}
-}
-}
-}
+
+
